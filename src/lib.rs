d648c00b4a7823a3ca1c9c0b1c0dd8fc7d31d15b
Norio4 <Norio4>
2021-08-29 14:29:11 +0900

    Add fetch current gateway IP

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![allow(clippy::needless_pass_by_value, clippy::trivially_copy_pass_by_ref)]
 use seed::{prelude::*, *};
+use serde::{Deserialize};
 
 // ------ ------
 //     Init
@@ -8,7 +9,8 @@ use seed::{prelude::*, *};
 fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
     Model {
         counter_1: 0,
-        counter_2: 0
+        counter_2: 0,
+        my_ip: String::new(),
     }
 }
 
@@ -18,9 +20,17 @@ fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
 
 struct Model {
     counter_1: i32,
-    counter_2: i32
+    counter_2: i32,
+    my_ip: String,
 }
 
+#[derive(Deserialize, Debug)]
+struct ResponsedIp {
+    message: String,
+    myip: String,
+    xforwardedfor: String,
+    ua: String,
+}
 
 // ------ ------
 //    Update
@@ -31,15 +41,41 @@ enum Msg {
     Decrement1,
     Increment2,
     Decrement2,
+    GetRequest,
+    FetchedIp(fetch::Result<ResponsedIp>),
 }
 
-fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+// fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
     match msg {
         Msg::Increment1 => model.counter_1 += 1,
         Msg::Decrement1 => model.counter_1 -= 1,
         Msg::Increment2 => model.counter_2 += 1,
         Msg::Decrement2 => model.counter_2 -= 1,
+        Msg::GetRequest => {
+            orders.skip().perform_cmd({
+                async {
+                    Msg::FetchedIp(get_ip().await)
+                }
+            });
+        }, Msg::FetchedIp(Ok(response_data)) => {
+           model.my_ip = response_data.myip;
+        }
+        Msg::FetchedIp(Err(error)) => {
+            log!(error);
+            model.my_ip = "Error Occurred in Fetching My Ip.".to_string();
+        }
     }
+}
+
+async fn get_ip() -> fetch::Result<ResponsedIp> {
+    Request::new("https://webtools.agedori.com/api/myip")
+        .method(Method::Get)
+        .fetch()
+        .await?
+        .check_status()?
+        .json()
+        .await
 }
 
 // ------ ------
@@ -77,7 +113,17 @@ fn view(model: &Model) -> Node<Msg> {
                 C!["uk-button uk-button-default uk-button-small"],
                 ev(Ev::Click, |_| Msg::Increment2), "加算2"
             ],
-        ]
+        ],
+        div![
+            C!["uk-divider-small"],
+        ],
+        div![
+            div![model.my_ip.clone()],
+            button![
+                C!["uk-button uk-button-default uk-button-small"],
+                ev(Ev::Click, |_| Msg::GetRequest), "IP取得"
+            ],
+        ],
     ]
 }
 
